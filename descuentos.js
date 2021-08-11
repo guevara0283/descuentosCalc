a3c430d6f10dfaec02b7cb00e303653a8d6d7c97
luis0283 <luis0283@outlook.com>
2021-08-11 16:00:53 -0600

    se agrega el valor del ahorro del cliente

--- descuentos.js
+++ descuentos.js
@@ -2,6 +2,8 @@ function resultado(){
     var precio=parseInt(document.getElementById('precio').value);
     var descuento=parseInt(document.getElementById('descuento').value);
     var resultado=(precio*descuento)/100;
+    var ahorro=(precio*descuento)/100;
    
+   document.getElementById('ahorro').innerText = 'Su ahorro es de: $' + (ahorro);
    document.getElementById('resultado').innerText = 'El precio total a pagar con descuento es: $' + (precio - resultado);
 }
